5ca7ed45c673288dc63ddc99f5d477f047f8f349
rwm77 <rwm77@192.168.1.142>
2021-02-07 23:59:13 -0600

    Start RepLevels class. Create database.

---
+++ STFC/src/net/robertwmurphy/stfc/RepLevels.java
@@ -0,0 +1,10 @@
+package net.robertwmurphy.stfc;
+
+public class RepLevels {
+	
+	private float minimumPoints;
+	private float maximumPoints;
+	private String repLevel;
+	private String faction;
+
+}
