9f88c2b88084d9a96958fa87c76966cfbb22ef87
rwm77 <rwm77@192.168.1.142>
2021-02-09 01:16:31 -0600

    Updated database and created RepLevels Getters and Setters

--- STFC/src/net/robertwmurphy/stfc/RepLevels.java
+++ STFC/src/net/robertwmurphy/stfc/RepLevels.java
@@ -2,9 +2,56 @@ package net.robertwmurphy.stfc;
 
 public class RepLevels {
 	
-	private float minimumPoints;
-	private float maximumPoints;
+	private int id;
+	private int minimumPoints;
+	private int maximumPoints;
 	private String repLevel;
 	private String faction;
+	
+	RepLevels() {
+		
+	}
+	
+	//Getters
+	public int getID() {
+		return id;
+	}
+	
+	public int getMinimumPoints() {
+		return minimumPoints;
+	}
+	
+	public int getMaximumPoints() {
+		return maximumPoints;
+	}
+	
+	public String getRepLevel() {
+		return repLevel;
+	}
+	
+	public String getFaction() {
+		return faction;
+	}
+	
+	//Setters
+	public void setID(int ID) {
+		this.id = ID;
+	}
+	
+	public void setMinimumPoints(int minPts) {
+		this.minimumPoints = minPts;
+	}
+	
+	public void setMaximumPoints(int maxPts) {
+		this.maximumPoints = maxPts;
+	}
+	
+	public void setRepLevel(String repLVL) {
+		this.repLevel = repLVL;
+	}
+	
+	public void setFaction(String faction) {
+		this.faction = faction;
+	}
 
 }
