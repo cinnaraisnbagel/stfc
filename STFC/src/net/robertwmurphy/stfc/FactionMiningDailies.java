16aac79b70882d9de74eea7bf78057dc879345ba
rwm77 <rwm77@192.168.1.142>
2021-02-10 23:41:03 -0600

    Created FactionMiningDailies class (setters/getters/constructor).

--- STFC/src/net/robertwmurphy/stfc/FactionMiningDailies.java
+++ STFC/src/net/robertwmurphy/stfc/FactionMiningDailies.java
@@ -1,5 +1,60 @@
 package net.robertwmurphy.stfc;
 
 public class FactionMiningDailies {
+	
+	private int id;
+	private String reputationLevel;
+	private String rssGrade;
+	private String rssLevelOne;
+	private String rssLevelTwo;
+	
+	//Constructor
+	FactionMiningDailies(int ID, String repLVL, String rssGrd, String rssLVLOne, String rssLVLTwo) {
+		setID(ID);
+		setrepLVL(repLVL);
+	}
+	
+	//Getters
+	public int getID() {
+		return id;
+	}
+	
+	public String getReputationLevel() {
+		return reputationLevel;
+	}
+	
+	public String getRSSGrade() {
+		return rssGrade;
+	}
+	
+	public String getRSSLevelOne() {
+		return rssLevelOne;
+	}
+	
+	public String getRSSLevelTwo() {
+		return rssLevelTwo;
+	}
+	
+	//Setters
+	public void setID(int ID) {
+		this.id = ID;
+	}
+	
+	public void setReputationLevel(String repLVL) {
+		this.reputationLevel = repLVL;
+	}
+	
+	public void setRSSGrade( String rssGrd) {
+		this.rssGrade = rssGrd;
+	}
+	
+	public void setRSSLevelOne(String rssLVLOne) {
+		this.rssLevelOne = rssLVLOne;
+	}
+	
+	public void setRSSLevelTwo(String rssLVLTwo) {
+		this.rssLevelTwo = rssLVLTwo;
+	}
+
 
 }
